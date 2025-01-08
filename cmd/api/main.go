953bb4cf23eb53fde7e07d349122879b7eee1211
Fabio Robledo Batoni de Godoy <fabio.godoy@orionpsp.com>
2025-01-08 10:37:55 -0300

    add go-chi render to convert json responses

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
 
 func main() {
@@ -11,6 +12,11 @@ func main() {
 	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
 		param := chi.URLParam(r, "productName")
 		w.Write([]byte(param))
+	})
+
+	r.Get("/json", func(w http.ResponseWriter, r *http.Request) {
+		obj := map[string]string{"message": "success"}
+		render.JSON(w, r, obj)
 	})
 
 	http.ListenAndServe(":3000", r)
