4bf70fe74012094571e7e78667f5883cb6f7e5b0
Fabio Robledo Batoni de Godoy <fabio.godoy@orionpsp.com>
2025-01-09 10:47:05 -0300

    global errors

--- internal/endpoints/campaigns_get.go
+++ internal/endpoints/campaigns_get.go
@@ -2,12 +2,9 @@ package endpoints
 
 import (
 	"net/http"
-
-	"github.com/go-chi/render"
 )
 
-func (h *Handler) CampaignGet(w http.ResponseWriter, r *http.Request) {
-
-	render.Status(r, 200)
-	render.JSON(w, r, h.CampaignService.Repository.Get())
+func (h *Handler) CampaignGet(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+	campaings, err := h.CampaignService.Repository.Get()
+	return campaings, 200, err
 }
