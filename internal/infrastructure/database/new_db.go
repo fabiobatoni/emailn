4b0f12174a2a44b5c74a439641f443bbcaf17e67
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2025-02-19 11:17:39 -0300

    add auto migrate

--- internal/infrastructure/database/new_db.go
+++ internal/infrastructure/database/new_db.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"emailn/internal/domain/campaign"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,5 +14,8 @@ func NewDb() *gorm.DB {
 	if err != nil {
 		panic("failed to connect database")
 	}
+
+	db.AutoMigrate(&campaign.Campaign{}, &campaign.Contact{})
+
 	return db
 }
