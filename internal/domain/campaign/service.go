2ae36e20a4e8f225b8992a13e17fab31a3ef27a2
Fabio Robledo Batoni de Godoy <fabio_batoni@hotmail.com>
2025-02-21 13:47:40 -0300

    gorm Preload

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -47,6 +47,7 @@ func (s *ServiceImp) GetBy(id string) (*contract.CampaignResponse, error) {
 		Name:    campaign.Name,
 		Content: campaign.Content,
 		Status:  campaign.Status,
+		AmountOfEmailsToSend: len(campaign.Contacts),
 	}, nil
 }
 
